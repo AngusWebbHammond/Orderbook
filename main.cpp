d11c3dde88fb40d8664345ef3f7cc89fc68bd507
AngusWebbHammond <anguswebbhammond@gmail.com>
2026-01-14 11:45:48 +0000

    Added an OrderbookValue and OrderbookKey struct, to change the implementation of the order book, such that all the information for buy/sell is in a single respective vector OrderbookKey is the head node for a linked list with OrderbookValue being the nodes

--- main.cpp
+++ main.cpp
@@ -46,6 +46,178 @@ private:
     Quantity m_quantity{};
     OrderType m_orderType{};
 };
+#define ORDERBOOK_MAP2
+#ifdef ORDERBOOK_MAP2
+
+struct OrderbookValue {
+    std::shared_ptr<Order> order;
+    OrderbookValue* nextOrder;
+};
+
+struct OrderbookKey {
+    Price price;
+    Quantity quantity;
+    OrderbookValue* orders;
+
+    OrderbookValue* find(OrderID id) {
+        OrderbookValue* ptr = orders;
+        while (ptr) {
+            if (ptr->order->getOrderID() == id) {
+                return ptr;
+            }
+            if (ptr->nextOrder) {
+                ptr = ptr->nextOrder;
+            }
+            else {
+                return nullptr;
+            }
+        }
+        return nullptr;
+    };
+
+    std::optional<OrderbookValue*> findPrevious(OrderID id) {
+        OrderbookValue* ptr = orders;
+        while (ptr) {
+            if (ptr->nextOrder) {
+                if (ptr->nextOrder->order->getOrderID() == id) {
+                    return ptr;
+                }
+
+                ptr = ptr->nextOrder;
+            }
+            else {
+                if (orders->order->getOrderID() == id) {
+                    return nullptr;
+                }
+                return std::nullopt;
+            }
+        }
+        return std::nullopt;
+    }
+
+    void pushBack(Order tempOrder) {
+        OrderbookValue* ptr = orders;
+        quantity += tempOrder.getQuantity();
+        while (ptr) {
+            if (ptr->nextOrder) {
+                ptr = ptr->nextOrder;
+            }
+            else {
+                OrderbookValue obv = { .order = std::make_shared<Order>(tempOrder), .nextOrder = nullptr };
+                ptr->nextOrder = new OrderbookValue(obv);
+                return;
+            }
+        }
+        OrderbookValue obv = { .order = std::make_shared<Order>(tempOrder), .nextOrder = nullptr };
+        orders = new OrderbookValue(obv);
+        return;
+    }
+
+    void erase(OrderID id) {
+        if (auto value = findPrevious(id); value) {
+            if (value.value()) {
+                auto* valueNext = value.value()->nextOrder;
+                value.value()->nextOrder = value.value()->nextOrder->nextOrder;
+                quantity -= valueNext->order->getQuantity();
+                delete valueNext;
+            }
+            else {
+                quantity -= orders->order->getQuantity();
+                OrderbookValue* orderValue = orders;
+                OrderbookValue* nextValue = orderValue->nextOrder;
+                delete orderValue;
+                orders = nextValue;
+            }
+        }
+        auto a = 1;
+    }
+
+    void clear() {
+        auto* ptr = orders;
+        while (ptr) {
+            auto* nextPtr = ptr->nextOrder;
+            if (!nextPtr) {
+                delete ptr;
+                ptr = nullptr;
+                return;
+            }
+            else if (nextPtr->nextOrder) {
+                delete ptr;
+                ptr = nextPtr;
+            }
+            else {
+                delete ptr;
+            }
+        }
+        orders = nullptr;
+        quantity = 0;
+    }
+};
+
+template<typename T>
+class OrderbookMap {
+public:
+    OrderbookMap() = default;
+    OrderbookMap(const OrderbookMap&) = delete;
+    OrderbookMap(OrderbookMap&&) = delete;
+    void operator=(const OrderbookMap&) = delete;
+    void operator=(OrderbookMap&&) = delete;
+    ~OrderbookMap() {
+        for (auto& key : m_orderbook) {
+            key.clear();
+        }
+    };
+
+    std::optional<std::shared_ptr<Order>> ifContainsGet(OrderID id) {
+        auto found = std::find_if(std::begin(m_orderbook), std::end(m_orderbook), [id](OrderbookKey item) {
+            if (auto found = item.find(id); found) {
+                return true;
+            }
+            return false;
+            }
+        );
+        if (found != std::end(m_orderbook)) {
+            auto f = m_orderbook.at(std::distance(std::begin(m_orderbook), found)).find(id);
+            std::shared_ptr<Order> res = f->order;
+            return res;
+        }
+        return std::nullopt;
+    }
+
+    OrderbookKey* ifContainsGet(Price price) {
+        auto found = std::find_if(std::begin(m_orderbook), std::end(m_orderbook), [price](OrderbookKey item) {return price == item.price;});
+        if (found != std::end(m_orderbook)) {
+            return &m_orderbook.at(std::distance(std::begin(m_orderbook), found));
+        }
+        return nullptr;
+    }
+
+    void insert(Order tempOrder) {
+        if (auto* item = ifContainsGet(tempOrder.getPrice()); item) {
+            item->pushBack(tempOrder);
+            return;
+        }
+        OrderbookValue obv = { .order{ std::make_shared<Order>(tempOrder) }, .nextOrder{ nullptr } };
+        auto* ptr = new OrderbookValue(std::move(obv));
+        OrderbookKey item = { .price{tempOrder.getPrice()}, .quantity{tempOrder.getQuantity()}, .orders{ptr} };
+        m_orderbook.push_back(item);
+    }
+
+    void ifContainsRemove(Price price) {
+        if (auto key = ifContainsGet(price); key) {
+            key->clear();
+            m_orderbook.erase(std::find_if(std::begin(m_orderbook), std::end(m_orderbook), [price](auto item) {return price == item.price;}));
+            return;
+        }
+        return;
+    }
+private:
+    std::vector<OrderbookKey> m_orderbook;
+};
+#else
+template<typename T>
+using OrderbookMap = std::map<Price, Quantity, T>;
+#endif
 
 class Orderbook {
 public:
@@ -57,6 +229,7 @@ public:
     ~Orderbook() = default;
 
     OrderID addOrder(Order& order) {
+#ifndef ORDERBOOK_MAP2
         if (order.getOrderType() == OrderType::BUY) {
             if (m_buyBook.contains(order.getPrice())) {
                 m_buyBook.at(order.getPrice()) += order.getQuantity();
@@ -77,9 +250,23 @@ public:
             m_allSellOrders.push_back(std::move(order));
             return order.getOrderID();
         }
+#else
+        if (order.getOrderType() == OrderType::BUY) {
+            m_buyBook.insert(order);
+        }
+        else {
+            m_sellBook.insert(order);
+        }
+        return order.getOrderID();
+#endif
     }
-
-    std::optional<Order> getOrder(OrderID orderID) {
+#ifndef ORDERBOOK_MAP2
+    std::optional<Order>
+#else
+    std::optional<std::shared_ptr<Order>>
+#endif
+        getOrder(OrderID orderID) {
+#ifndef ORDERBOOK_MAP2
         auto sellOrder = std::find_if(std::begin(m_allSellOrders), std::end(m_allSellOrders), [orderID](Order& order) { return order.getOrderID() == orderID;});
         auto buyOrder = std::find_if(std::begin(m_allBuyOrders), std::end(m_allBuyOrders), [orderID](Order& order) { return order.getOrderID() == orderID;});
         if (sellOrder != std::end(m_allSellOrders)) {
@@ -89,9 +276,23 @@ public:
             return m_allBuyOrders.at(std::distance(std::begin(m_allBuyOrders), buyOrder));
         }
         else return std::nullopt;
+#else
+        auto sellOrder = m_sellBook.ifContainsGet(orderID);
+        auto buyOrder = m_buyBook.ifContainsGet(orderID);
+        if (sellOrder) {
+            return sellOrder.value();
+        }
+        else if (buyOrder) {
+            return buyOrder.value();
+        }
+        else {
+            return std::nullopt;
+        }
+#endif
     }
 
     bool cancelOrder(OrderID orderID) {
+#ifndef ORDERBOOK_MAP2
         if (auto order = getOrder(orderID); order.has_value()) {
             if (order.value().getOrderType() == OrderType::BUY) {
                 auto buyOrder = std::find(std::begin(m_allBuyOrders), std::end(m_allBuyOrders), order.value());
@@ -106,24 +307,53 @@ public:
             return true;
         }
         return false;
+#else
+        if (auto order = getOrder(orderID); order) {
+            if (order.value()->getOrderType() == OrderType::BUY) {
+                OrderbookKey* item = m_buyBook.ifContainsGet(order.value()->getPrice());
+                item->erase(orderID);
+                return true;
+            }
+            else {
+                auto item = m_sellBook.ifContainsGet(order.value()->getPrice());
+                item->erase(orderID);
+                return true;
+            }
+        }
+        else {
+            return false;
+        }
+#endif
     }
 
 private:
-    std::map<Price, Quantity, std::greater<Price>> m_buyBook{};
-    std::map<Price, Quantity, std::less<Price>> m_sellBook{};
+#ifndef ORDERBOOK_MAP2
+    // Could join the below into a std::map<std::pair<Price, Quantity>, std::vector<Order>, T>
+    OrderbookMap<std::less<>> m_buyBook{};
+    OrderbookMap<std::greater<>> m_sellBook{};
     std::vector<Order> m_allBuyOrders{};
     std::vector<Order> m_allSellOrders{};
+#else
+    OrderbookMap<std::less<>> m_buyBook{};
+    OrderbookMap<std::greater<>> m_sellBook{};
+#endif
 };
 
 int main() {
     Orderbook orderbook;
 
-    Order orderBuy(10, 10, OrderType::BUY);
-    Order orderSell(20, 31, OrderType::SELL);
-    orderbook.addOrder(orderBuy);
-    orderbook.addOrder(orderSell);
+    Order orderBuy1(10, 10, OrderType::BUY);
+    Order orderBuy2(12, 10, OrderType::BUY);
+    Order orderSell1(20, 31, OrderType::SELL);
+    Order orderSell2(20, 32, OrderType::SELL);
+    orderbook.addOrder(orderBuy1);
+    orderbook.addOrder(orderBuy2);
+    orderbook.addOrder(orderSell1);
+    orderbook.addOrder(orderSell2);
 
     orderbook.cancelOrder(0);
+    orderbook.cancelOrder(2);
 
+    orderbook.addOrder(orderBuy1);
     return 0;
 }
