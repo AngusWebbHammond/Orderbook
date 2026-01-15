5f28f4301d4443b050670ef68a080b174cb716d4
AngusWebbHammond <anguswebbhammond@gmail.com>
2026-01-15 22:15:46 +0000

    Updated Todos

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include "src/Orderbook.h"
 
-// TODO implement a way of issuing trades
+// -------------------------------------------------------------------------------
+// Todo list
+// -------------------------------------------------------------------------------
+// TODO implement a way of issuing trades 
 //      This should allow the trades to go off the best price on each orderbook
 // TODO test this with real market data
+// TODO Add a csv parser for incoming trades, for example:
+//      "S, 10, 10, ID" : Sell 10 at 10 price
+//      "B, 100, 9, ID" : Buy 100 at 9 price
+// TODO Implement a way of switching between FIFO and Pro-Rata trades
+// TODO Add a UserID system, which will tie a user to a trade
+// TODO Add robust tests
+// -------------------------------------------------------------------------------
 
 int main() {
     Orderbook::Orderbook orderbook;
